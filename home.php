014f4196fc30f15985b76fb29d8a1bfb31b85893
scouttroop439 <scouttroop439@gmail.com>
2014-02-01 05:09:38 -0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<head>
+	<!-- login -->
+	<script language="javascript">
+		<!--hide
+		var enteredPassword;
+		var correctPassword = "troop439";
+		enteredPassword = prompt('Please enter the password','troop439');
+		if (enteredPassword != correctPassword){
+			window.location="index.php";
+		}
+		//-->
+	</script>
+	<title>Boy Scout Troop 439's Official Website.</title>
+	
+	<!-- meta -->
+	<meta name="description" content="Boy Scout Troop 439's Official Website">
+	<meta name="keywords" content="boyscout,scouting,boy,scout,scouting,merit,badge,merit badge,BSA,america,youth,organization">
+	<meta name="author" content="Joseph Winger">
+	
+	<!-- stylesheets -->
+	<link rel="stylesheet" type="text/css" href="main-style.css">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	
+	<!-- icons -->
+    <link rel="icon" type="image/png" href="images/logo.png">
+</head>
+<body>
+	<div id="pageTop">
+		<div id="pageTopLogo">
+			<img src="images/logo.png" width="45px" height="45px"></img>
+		</div>
+		<div id="pageTopSeparator">
+			<img src="images/vertical-header-separator.png"></img>
+		</div>
+		<div id="pageTopText">
+			<a href="#top">Boy Scout Troop 439</a>
+		</div>
+		<div id="navBar">
+			<a href="index.php">Back to Front Page</a>
+		</div>
+	</div>
+	<div id="pageMiddle">
+		<div id="pageMiddleIntro">
+			<i class="fa fa-camera-retro"></i><h1>Boy Scout Troop 439</h1>
+			<center><p>Hello scouts, and welcome to troop 439's official web site! Below you will notice our troop's blog; a new way to get information on new and upcoming events. The green bar above has some useful links. Feel free to explore and use this website in any way that will help. Thank You :)</p><br></center>
+		</div>
+		<div id="blog">
+			<script type="text/javascript" src="http://scouttroop439.tumblr.com/js"></script>
+		</div>
+	</div>
+	<div id="pageBottom"></div>
+</body>
+	
+<footer>
+	
+</footer>
