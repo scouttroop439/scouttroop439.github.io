ec9fe3efb900fd5f93f41491c1fb6c3fa75bafca
scouttroop439 <scouttroop439@gmail.com>
2014-02-01 05:09:13 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<head>
+	<title>Boy Scout Troop 439's Official Website.</title>
+	
+	<!-- meta -->
+	<meta name="description" content="Boy Scout Troop 439's Official Website">
+	<meta name="keywords" content="boyscout,scouting,boy,scout,scouting,merit,badge,merit badge,BSA,america,youth,organization">
+	<meta name="author" content="Joseph Winger">
+	
+	<!-- stylesheets -->
+	<link rel="stylesheet" type="text/css" href="main-style.css">
+	<link href="www.netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	
+	<!-- icons -->
+    <link rel="icon" type="image/png" href="images/logo.png">
+</head>
+<body>
+	<div id="pageTop">
+		<div id="pageTopLogo">
+			<img src="images/logo.png" width="45px" height="45px"></img>
+		</div>
+		<div id="pageTopSeparator">
+			<img src="images/vertical-header-separator.png"></img>
+		</div>
+		<div id="pageTopText">
+			<a href="#top">Boy Scout Troop 439</a>
+		</div>
+		<div id="navBar">
+			<ul>
+				<a href="home.php">Login to the Home Page</a>
+			</ul>
+		</div>
+	</div>
+	<div id="pageMiddle">
+		<h2>Boy Scout Troop 439</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ligula et tortor pulvinar tempus a in urna. Aliquam in placerat mauris. Proin lacus enim, eleifend placerat ante non, tempor accumsan nisl. Aliquam laoreet suscipit scelerisque. Mauris porta sapien quis scelerisque varius. Nullam rhoncus congue enim, id tincidunt mauris dapibus nec. Nulla dapibus risus ac massa dictum, et semper lacus malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent quis consequat ante. Vestibulum vestibulum dictum orci, id venenatis nulla lobortis sit amet. Nullam sollicitudin magna vitae aliquet eleifend. Ut hendrerit, nibh id sodales rhoncus, leo nulla consectetur justo, non venenatis orci libero id tellus.
+		</p>
+		<br/><br/>
+		<div id="column1">
+			<h2>Title 1</h2>
+			<p>Maecenas placerat nisl ut venenatis egestas. Quisque vel tortor ipsum. Vivamus rutrum mollis diam, sit amet dapibus purus fermentum laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent blandit lorem sapien, vel adipiscing risus cursus id. Pellentesque egestas ultrices leo et tincidunt. Donec sit amet venenatis massa. Proin tempor, urna a porta sodales, ligula mi vehicula dolor, venenatis tempus ante odio ac arcu. Sed nec dolor dapibus, lacinia nisl at, mattis velit. Praesent in congue lectus, id adipiscing justo. Suspendisse dignissim, eros eget mollis fringilla, eros risus tempus nisl, vitae bibendum nibh nunc et nunc. Fusce et ipsum tempus, sodales diam malesuada, egestas velit. Nam ornare dictum egestas. Nunc ut eros quis arcu pellentesque volutpat eget dignissim lorem.</p>
+		</div>
+		<div id="column2">
+			<h2>Title 2</h2>
+			<p>Mauris molestie, velit dapibus cursus eleifend, tellus orci dictum mauris, quis eleifend felis nisi eu justo. Aliquam nisi mauris, vehicula non euismod at, posuere sed nulla. Suspendisse vel purus neque. Donec a dictum purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. In non ipsum odio. Ut quis leo magna. Phasellus eu laoreet lectus, sed ultricies tellus. Aliquam erat volutpat. In tempus tortor ac dolor fermentum, nec dapibus nibh egestas. Interdum et malesuada fames ac ante ipsum primis in faucibus.Mauris molestie, velit dapibus cursus eleifend, tellus orci dictum mauris, quis eleifend felis nisi eu justo. Aliquam nisi mauris, vehicula non euismod at, posuere sed nulla. Suspendisse vel purus neque. Donec a dictum purus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+		</div>
+	</div>
+	<div id="pageBottom"></div>
+</body>
+	
+<footer>
+	
+</footer>
